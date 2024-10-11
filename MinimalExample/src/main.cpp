3f60b5808b3b9a6e67f4d4b792eb8ebc47d8e7b9
Yuri Ghini <yuri.g93@gmail.com>
2024-10-11 12:04:35 +0200

    Change indentation

--- MinimalExample/src/main.cpp
+++ MinimalExample/src/main.cpp
@@ -5,29 +5,29 @@
 // Clears the current window and draws a triangle.
 void display() {
 
-	// Set every pixel in the frame buffer to the current clear color.
-	glClear(GL_COLOR_BUFFER_BIT);
+    // Set every pixel in the frame buffer to the current clear color.
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	// Drawing is done by specifying a sequence of vertices.  The way these
-	// vertices are connected (or not connected) depends on the argument to
-	// glBegin.  GL_POLYGON constructs a filled polygon.
-	glBegin(GL_POLYGON);
+    // Drawing is done by specifying a sequence of vertices.  The way these
+    // vertices are connected (or not connected) depends on the argument to
+    // glBegin.  GL_POLYGON constructs a filled polygon.
+    glBegin(GL_POLYGON);
 
-	// For each vertex we can specify the color and the position. glColor3f uses the RGB notation
-	// glVertex3f accepts x,y,z as parameters. The center of the coordinate system is in the middle of the window
-	glColor3f(1, 0, 0);
-	glVertex3f(0, 0.6, 0);
+    // For each vertex we can specify the color and the position. glColor3f uses the RGB notation
+    // glVertex3f accepts x,y,z as parameters. The center of the coordinate system is in the middle of the window
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0.6, 0);
 
-	glColor3f(0, 1, 0);
-	glVertex3f(-0.6f, -0.6f, 0.0f);
+    glColor3f(0, 1, 0);
+    glVertex3f(-0.6f, -0.6f, 0.0f);
 
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.6f, -0.6f, 0.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.6f, -0.6f, 0.0f);
 
-	glEnd();
+    glEnd();
 
-	// Flush drawing command buffer to make drawing happen as soon as possible.
-	glFlush();
+    // Flush drawing command buffer to make drawing happen as soon as possible.
+    glFlush();
 }
 
 int main(int argc, char** argv)
@@ -35,19 +35,19 @@ int main(int argc, char** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
-	// Position window at (80,80)-(480,380) and give it a title.
-	glutInitWindowPosition(80, 80);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("A Simple Triangle");
+    // Position window at (80,80)-(480,380) and give it a title.
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("A Simple Triangle");
 
-	// Tell GLUT that whenever the main window needs to be repainted that it
-	// should call the function display().
-	glutDisplayFunc(display);
+    // Tell GLUT that whenever the main window needs to be repainted that it
+    // should call the function display().
+    glutDisplayFunc(display);
 
-	// Tell GLUT to start reading and processing events.  This function
-	// never returns; the program only exits when the user closes the main
-	// window or kills the process.
-	glutMainLoop();
+    // Tell GLUT to start reading and processing events.  This function
+    // never returns; the program only exits when the user closes the main
+    // window or kills the process.
+    glutMainLoop();
 
     return 0;
 }
